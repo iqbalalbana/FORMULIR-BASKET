a42b33e13f81c5cbb5a1ff7b5632301e4c73f32f
Iqbal_Albana <Iqbalalbana16@gmail.com>
2016-09-15 11:49:17 +0700

    - Commit pertama : Nama dan Umur (Edit Text) - Commit kedua : Jenis Kelamin (Radio Button) - Commit ketiga : Asal Kota (Spinner) - Commit keempat : Posisi yang diinginkan (Checkbox) - Commit finishing : Background
    - Commit Revisi

--- app/src/main/java/id/sch/smktelkom_mlg/tugas01/xirpl1021/formulirbasket/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/tugas01/xirpl1021/formulirbasket/MainActivity.java
@@ -14,13 +14,13 @@ import android.widget.TextView;
 
 
 public class MainActivity extends AppCompatActivity implements CompoundButton.OnCheckedChangeListener {
-    EditText etNama;
-    EditText etTahun;
+
+    EditText etNama, etTahun;
     RadioGroup rgJk;
     Spinner spKota;
     CheckBox cb1, cb2, cb3, cb4;
     Button bOk;
-    TextView tvHasil, tvPosisi;
+    TextView tvHasil1, tvHasil2, tvHasil3, tvHasil4, tvHasil5, tvPosisi;
     int nPosisi;
 
     @Override
@@ -41,61 +41,75 @@ public class MainActivity extends AppCompatActivity implements CompoundButton.On
         cb3.setOnCheckedChangeListener(this);
         cb4.setOnCheckedChangeListener(this);
         bOk = (Button) findViewById(R.id.buttonOk);
-        tvHasil = (TextView) findViewById(R.id.textViewHasil);
+        tvHasil1 = (TextView) findViewById(R.id.textViewHasil1);
+        tvHasil2 = (TextView) findViewById(R.id.textViewHasil2);
+        tvHasil3 = (TextView) findViewById(R.id.textViewHasil3);
+        tvHasil4 = (TextView) findViewById(R.id.textViewHasil4);
+        tvHasil5 = (TextView) findViewById(R.id.textViewHasil5);
         tvPosisi = (TextView) findViewById(R.id.textViewPosisi);
 
-        bOk.setOnClickListener(new View.OnClickListener() {
+        findViewById(R.id.buttonOk).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 doClick();
             }
         });
+
     }
 
     private void doClick() {
         String nama = etNama.getText().toString();
-        String tahun = etTahun.getText().toString();
-        String hasil = null;
-        String asal = spKota.getSelectedItem().toString();
-        String posisi = "\nPosisi Anda           : ";
+        String umur = etTahun.getText().toString();
+        String Jenis = null;
+        String Posisi = "Posisi            : ";
+        int startlen = Posisi.length();
 
-        int startlen = posisi.length();
-        if (cb1.isChecked()) posisi += cb1.getText() + ",";
-        if (cb2.isChecked()) posisi += cb2.getText() + ",";
-        if (cb3.isChecked()) posisi += cb3.getText() + ",";
-        if (cb4.isChecked()) posisi += cb4.getText();
+        if (nama.isEmpty()) {
+            etNama.setError("Nama belum diisi");
+        } else if (nama.length() <= 4) {
+            etNama.setError("Nama minimal 4 karakter");
+        } else {
+            etNama.setError(null);
+        }
+        tvHasil1.setText("Nama                      : " + nama);
 
-        if (posisi.length() == startlen) posisi += "Tidak ada pada pilihan";
+        if (umur.isEmpty()) {
+            etTahun.setError("Umur belum diisi");
+        } else if (umur.length() != 2) {
+            etTahun.setError("Format Umur Anda Salah");
+        } else {
+            etTahun.setError(null);
+        }
+        tvHasil2.setText("Umur                       : " + umur);
 
+        tvHasil3.setText("Asal                         : " + "Kota " + spKota.getSelectedItem().toString());
         if (rgJk.getCheckedRadioButtonId() != -1) {
             RadioButton rb = (RadioButton)
                     findViewById(rgJk.getCheckedRadioButtonId());
-            hasil = rb.getText().toString();
+            Jenis = rb.getText().toString();
+        }
+        if (Jenis == null) {
+            tvHasil4.setText("Jenis Kelamin        : -");
+        } else {
+            tvHasil4.setText("Jenis Kelamin        : " + Jenis);
         }
 
-        if (nama.isEmpty()) {
-            etNama.setError("Nama Belum Diisi");
-        } else if (nama.length() < 3) {
-            etNama.setError("Nama Minimal 3 Karakter");
-        } else {
-            etNama.setError(null);
-        }
+        if (cb1.isChecked()) Posisi += cb1.getText() + ", ";
+        if (cb2.isChecked()) Posisi += cb2.getText() + ", ";
+        if (cb3.isChecked()) Posisi += cb3.getText() + ". ";
 
-        if (tahun.isEmpty()) {
-            etTahun.setError("Tahun Belum Diisi");
-        } else if (tahun.length() > 2) {
-            etTahun.setError("Format Umur Salah");
-        } else {
-            etNama.setError(null);
-        }
-        tvHasil.setText("**************** ( TSBC ) ****************" + "\nNama         : " + nama + " \nUmur         : " + tahun +
-                "\nJenis Kelamin     : " + hasil + "\nAsal Kota       : " + asal + posisi);
-        }
+        if (Posisi.length() == startlen) Posisi += "Tidak ada pada Pilihan";
+        tvHasil5.setText(Posisi);
+
+
+    }
 
     @Override
     public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
         if (isChecked) nPosisi += 1;
         else nPosisi -= 1;
-        tvPosisi.setText("Hobi (" + nPosisi + " terpilih)");
+
+        tvHasil5.setText("Posisi            : ");
+
     }
 }
